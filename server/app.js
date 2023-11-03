4298ecd5c91daa3681d35860063c142cdd0be429
Keem <mr.sahapap@gmail.com>
2023-11-03 15:38:12 +0700

    fix: change importing Use Dynamic Imports in app.js

--- server/app.js
+++ server/app.js
@@ -1,17 +1,7 @@
 import express from "express"; //const express = require("express"); import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import authRouter from "./apps/auth.js";
 import dotenv from "dotenv";
-import registRouter from "./Website_Router/registRouter.js";
-import uploadRouter from "./Website_Router/uploadRouter.js";
-import jobRouter from "./apps/jobs.js";
-import categoryRouter from "./apps/category.js";
-import typeRouter from "./apps/type.js";
-import applyappliRouter from "./apps/applyappli.js";
-import jobAppRouter from "./apps/jobApp.js";
-import profileRouter from "./apps/profile.js";
-import followingRouter from "./apps/following.js";
 
 async function init() {
   dotenv.config();
@@ -29,17 +19,28 @@ async function init() {
 
   app.use(bodyParser.json());
 
+  const authRouter = await import("./apps/auth.js");
+  const registRouter = await import("./Website_Router/registRouter.js");
+  const uploadRouter = await import("./Website_Router/uploadRouter.js");
+  const jobRouter = await import("./apps/jobs.js");
+  const categoryRouter = await import("./apps/category.js");
+  const typeRouter = await import("./apps/type.js");
+  const applyappliRouter = await import("./apps/applyappli.js");
+  const jobAppRouter = await import("./apps/jobApp.js");
+  const profileRouter = await import("./apps/profile.js");
+  const followingRouter = await import("./apps/following.js");
+
   // router
-  app.use("/regist", registRouter);
-  app.use("/auth", authRouter);
-  app.use("/upload", uploadRouter);
-  app.use("/jobs", jobRouter);
-  app.use("/jobapp", jobAppRouter);
-  app.use("/following", followingRouter);
-  app.use("/category", categoryRouter);
-  app.use("/type", typeRouter);
-  app.use("/apply", applyappliRouter);
-  app.use("/profile", profileRouter);
+  app.use("/regist", registRouter.default);
+  app.use("/auth", authRouter.default);
+  app.use("/upload", uploadRouter.default);
+  app.use("/jobs", jobRouter.default);
+  app.use("/jobapp", jobAppRouter.default);
+  app.use("/following", followingRouter.default);
+  app.use("/category", categoryRouter.default);
+  app.use("/type", typeRouter.default);
+  app.use("/apply", applyappliRouter.default);
+  app.use("/profile", profileRouter.default);
   // router
 
   // test route
