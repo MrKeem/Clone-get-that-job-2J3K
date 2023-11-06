fef40f81fd1aa0b1c857e5a634645c6d5441fc50
Keem <mr.sahapap@gmail.com>
2023-11-06 11:50:22 +0700

    fix: create vercel.json

--- server/app.js
+++ server/app.js
@@ -13,49 +13,49 @@ import jobAppRouter from "./apps/jobApp.js";
 import profileRouter from "./apps/profile.js";
 import followingRouter from "./apps/following.js";
 
-dotenv.config();
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.API_KEY,
-//   api_secret: process.env.API_SECRET,
-//   secure: true,
-// });
-const app = express();
-// const port = 4000;
-const port = process.env.PORT || 4000;
+async function init() {
+  dotenv.config();
+  // cloudinary.config({
+  //   cloud_name: process.env.CLOUD_NAME,
+  //   api_key: process.env.API_KEY,
+  //   api_secret: process.env.API_SECRET,
+  //   secure: true,
+  // });
+  const app = express();
+  // const port = 4000;
+  const port = process.env.PORT || 4000;
 
-app.use(cors());
+  app.use(cors());
 
-app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-// router
-app.use("/regist", registRouter);
-app.use("/auth", authRouter);
-app.use("/upload", uploadRouter);
-app.use("/jobs", jobRouter);
-app.use("/jobapp", jobAppRouter);
-app.use("/following", followingRouter);
-app.use("/category", categoryRouter);
-app.use("/type", typeRouter);
-app.use("/apply", applyappliRouter);
-app.use("/profile", profileRouter);
-// router
+  // router
+  app.use("/regist", registRouter);
+  app.use("/auth", authRouter);
+  app.use("/upload", uploadRouter);
+  app.use("/jobs", jobRouter);
+  app.use("/jobapp", jobAppRouter);
+  app.use("/following", followingRouter);
+  app.use("/category", categoryRouter);
+  app.use("/type", typeRouter);
+  app.use("/apply", applyappliRouter);
+  app.use("/profile", profileRouter);
+  // router
 
-// test route
-app.get("/", (req, res) => {
-  res.json("Hello Get That Job!");
-  res.send("Hello Get That Job!");
-});
-
-app.get("*", (req, res) => {
-  res.status(404).send("Not found, Check route");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
   // test route
-});
+  app.get("/", (req, res) => {
+    res.json("Hello Get That Job!");
+    res.send("Hello Get That Job!");
+  });
+
+  app.get("*", (req, res) => {
+    res.status(404).send("Not found, Check route");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    // test route
+  });
+}
 
 init();
-
-export default app;
